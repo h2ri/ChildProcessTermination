3b17be776b63ec4af9950d3c8c5184088e13f597
Hari <hariprasad@pharmeasy.in>
2017-08-07 18:40:33 +0530

    first commit

--- ChildProcessKill.py
+++ ChildProcessKill.py
@@ -1,21 +1,20 @@
 import signal
 import psutil
-import os
 
 MEMORY_IN_KB = 4000
 
-def kill_child_processes(parent_pid):
+def terminate_child_process(parent_pid):
     try:
       parent = psutil.Process(parent_pid)
     except psutil.NoSuchProcess:
       return
-    children = parent.children(recursive=True)
-    print(children)
+    children = parent.children()
     for process in children:
       if process.memory_info().rss/1024 > MEMORY_IN_KB :
+          print(process)
           process.terminate()
 
 file = open("input.txt", "r")
 with open("input.txt") as f:
     parent_process_id = int(f.readlines()[0])
-kill_child_processes(parent_process_id)
+terminate_child_process(parent_process_id)
